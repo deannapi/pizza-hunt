02e567cc23b8540f4d9177f5804a0f9ba06b4b8b
deannapi <deannapi.mart@gmail.com>
2020-07-13 13:32:15 -0500

    add crud pizza routes

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// Create the Pizzal model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza mode
+module.exports = Pizza;
